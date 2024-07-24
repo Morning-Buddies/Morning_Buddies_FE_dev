7dbd18c5a829587012718b0e66b4e4740bf3cf5b
Anas <wg12181218@gmail.com>
2024-07-24 21:14:08 +0900

    FEAT: 회원가입시 회원정보 API POST

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:morning_buddies/screens/home/home_main.dart';
 import 'package:morning_buddies/screens/onboarding/onboarding_signin.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -7,6 +8,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
---
+++ lib/models/group_controller.dart
@@ -0,0 +1,8 @@
+// import 'package:get/get.dart';
+// import 'package:morning_buddies/screens/home/home_profile.dart';
+
+// class Groupcontroller extends GetxController {
+//   final groups = <GroupStatus>[
+    
+//   ].obs
+// }
